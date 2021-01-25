660695187b826ed145aab67e81677bbc5e558efd
iranimij <iranimij@gmail.com>
2021-01-25 20:55:05 +0330

    Burger component completion

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Aux from '../../hoc/auxiliary';
-
+import Burger from '../../components/Burger/Burger';
 const BurgerBuilder = (props) => {
   return (
       <Aux>
-          <div>Burger</div>
+          <Burger />
           <div>Burger Controls</div>
       </Aux>
   );
